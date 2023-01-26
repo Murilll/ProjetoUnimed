1677ac28ae5d367780ec47892c6d375a14e96925
Murilll <murilosertorio25@gmail.com>
2023-01-26 12:44:03 -0300

    ._.

---
+++ ProjetoUni/PacienteUnimed/js/marcarConsulta.js
@@ -0,0 +1,9 @@
+function aumentarCard(){
+    let editar = document.getElementById("cardMedico");
+    editar.style.cssText = "width: 115%;"
+}
+
+function fecharCard(){
+    let editar = document.getElementById("cardMedico");
+    editar.style.cssText = "width: 100%;";
+}
